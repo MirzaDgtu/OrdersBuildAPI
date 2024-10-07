12131dbe5b39fd4d390abf455d61df36c11d2cf0
Mirza Ali <mirzyali@mail.ru>
2024-10-07 08:16:36 +0300

    add model user

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,1 +1,13 @@
 package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name      string `json:"name"`
+	FirstName string `gorm:"column:firstname" json:"firstname" required`
+	LastName  string `gorm:"column:lastname" json:"lastname" required`
+	Email     string `gorm:"column:email" json:"email" required`
+	Pass      string `gorm:"column:pass" json:"pass" required`
+	LoggedIn  bool   `gorm:"column:loggedin" json:"loggedin"`
+}
